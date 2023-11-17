c5acde1fe9dd99ba1ab1c89314a07976bdb00267
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2023-11-17 19:47:06 +0330

    test: locale switching

---
+++ src/components/Language.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="my-4">
+    <span class="locale" title="فارسی" @click="$i18n.locale = 'fa'">
+      فارسی
+      <img src="https://flagsapi.com/IR/flat/24.png" />
+    </span>
+
+    <span class="locale" title="English" @click="$i18n.locale = 'en'">
+      English
+      <img src="https://flagsapi.com/US/flat/24.png" />
+    </span>
+  </div>
+</template>
+
+<style scoped>
+.locale {
+  cursor: pointer;
+}
+</style>
